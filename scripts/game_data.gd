8874ffc37fca6ae217bae5d98a827eed5d25e281
langtao <langtao0415@outlook.com>
2026-03-03 22:51:21 +0800

    feat(phase3): 添加武器选择和全局数据

---
+++ scripts/game_data.gd
@@ -0,0 +1,28 @@
+extends Node
+
+var selected_weapon: String = "rifle"
+var player_stats = {
+	"max_hp": 100.0,
+	"hp_regen": 0.0,
+	"damage_mult": 1.0,
+	"attack_speed_mult": 1.0,
+	"move_speed_mult": 1.0,
+	"tower_mult": 1.0
+}
+var coins: int = 50
+var current_wave: int = 0
+var tower_inventory = []
+
+func reset():
+	selected_weapon = "rifle"
+	player_stats = {
+		"max_hp": 100.0,
+		"hp_regen": 0.0,
+		"damage_mult": 1.0,
+		"attack_speed_mult": 1.0,
+		"move_speed_mult": 1.0,
+		"tower_mult": 1.0
+	}
+	coins = 50
+	current_wave = 0
+	tower_inventory = []
---
+++ scripts/weapon_select.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready():
+	$VBoxContainer/RifleButton.pressed.connect(_on_rifle_selected)
+	$VBoxContainer/ShotgunButton.pressed.connect(_on_shotgun_selected)
+
+func _on_rifle_selected():
+	GameData.selected_weapon = "rifle"
+	start_game()
+
+func _on_shotgun_selected():
+	GameData.selected_weapon = "shotgun"
+	start_game()
+
+func start_game():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
