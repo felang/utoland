811a7d8d7c2a6015b77471275481b6e323daed5c
langtao <langtao0415@outlook.com>
2026-03-03 20:18:05 +0800

    fix(phase1): 添加HUD玩家引用失败警告

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -7,6 +7,8 @@ var player: Node2D = null
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
+	if not player:
+		push_warning("HUD: Player node not found in 'player' group")
 
 func _process(_delta):
 	if player:
