538584ceb3a374159abc9b005b33dc0a37d8ff61
langtao <langtao0415@outlook.com>
2026-03-03 21:43:43 +0800

    fix(phase2): 更新豌豆射手脚本使用DetectArea和ShootTimer节点

--- scripts/tower_shooter.gd
+++ scripts/tower_shooter.gd
@@ -1,32 +1,38 @@
 extends Tower
 
-@export var attack_range: float = 250.0
+@export var attack_range: float = 300.0
 @export var attack_damage: float = 10.0
 @export var attack_rate: float = 1.0
 
-var attack_timer: float = 0.0
-var bullet_scene = preload("res://scenes/bullet.tscn")
+@onready var detect_area: Area2D = $DetectArea
+@onready var shoot_timer: Timer = $ShootTimer
 
-func _process(delta):
-	attack_timer -= delta
-	if attack_timer <= 0:
-		shoot_nearest_enemy()
+const BULLET_SCENE = preload("res://scenes/bullet.tscn")
+
+func _ready():
+	super._ready()
+	shoot_timer.wait_time = attack_rate
+	shoot_timer.timeout.connect(_on_shoot_timer_timeout)
+	shoot_timer.start()
+
+func _on_shoot_timer_timeout():
+	shoot_nearest_enemy()
 
 func shoot_nearest_enemy():
-	var enemies = get_tree().get_nodes_in_group("enemies")
+	var enemies = detect_area.get_overlapping_bodies()
 	var closest = null
 	var min_dist = attack_range
-	
+
 	for enemy in enemies:
-		var dist = global_position.distance_to(enemy.global_position)
-		if dist < min_dist:
-			min_dist = dist
-			closest = enemy
-	
+		if enemy.is_in_group("enemies"):
+			var dist = global_position.distance_to(enemy.global_position)
+			if dist < min_dist:
+				min_dist = dist
+				closest = enemy
+
 	if closest:
-		var bullet = bullet_scene.instantiate()
+		var bullet = BULLET_SCENE.instantiate()
 		bullet.global_position = global_position
 		bullet.direction = global_position.direction_to(closest.global_position)
 		bullet.damage = attack_damage
 		get_parent().add_child(bullet)
-		attack_timer = attack_rate
