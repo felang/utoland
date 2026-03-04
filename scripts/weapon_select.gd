1e04d0ca356b22e4133902f7b966e1dcf17d1c44
langtao <langtao0415@outlook.com>
2026-03-05 00:18:15 +0800

    feat: 武器选择后跳转到地图选择

--- scripts/weapon_select.gd
+++ scripts/weapon_select.gd
@@ -13,4 +13,4 @@ func _on_shotgun_selected():
 	start_game()
 
 func start_game():
-	get_tree().change_scene_to_file("res://scenes/placement.tscn")
+	get_tree().change_scene_to_file("res://scenes/ui/map_select.tscn")
