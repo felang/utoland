719147e56cf9ae36264265d16b3ea04a299660e0
langtao <langtao0415@outlook.com>
2026-03-04 22:09:41 +0800

    feat: 添加角色选择界面

---
+++ scripts/character_selection.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var warrior_button: Button = $CharacterContainer/WarriorButton
+@onready var ranger_button: Button = $CharacterContainer/RangerButton
+@onready var tank_button: Button = $CharacterContainer/TankButton
+
+func _ready() -> void:
+	warrior_button.pressed.connect(_on_character_selected.bind("warrior"))
+	ranger_button.pressed.connect(_on_character_selected.bind("ranger"))
+	tank_button.pressed.connect(_on_character_selected.bind("tank"))
+
+func _on_character_selected(character_id: String) -> void:
+	GameData.current_character = character_id
+	print("选择角色: ", character_id)
+	get_tree().change_scene_to_file("res://scenes/weapon_selection.tscn")
