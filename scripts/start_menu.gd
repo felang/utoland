a4fc225acbc271b565194ea0b02e3a6a56900f49
langtao <langtao0415@outlook.com>
2026-03-04 22:14:37 +0800

    feat: 调整场景流程，插入角色选择界面

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -4,4 +4,4 @@ func _ready():
 	$VBoxContainer/StartButton.pressed.connect(_on_start_pressed)
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/ui/weapon_select.tscn")
+	get_tree().change_scene_to_file("res://scenes/character_selection.tscn")
