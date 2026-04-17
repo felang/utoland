5bb4332bc7677854f9eadfca1f326df2a1c1772e
langtao <langtao0415@outlook.com>
2026-04-17 17:17:06 +0800

    refactor(#4): 删除旧随机地图生成系统(Prefab/Autotiler/Generator Config)

--- scripts/core/game_config.gd
+++ scripts/core/game_config.gd
@@ -137,7 +137,6 @@ var shop: ShopConfigData = null
 var shop_config: ShopConfig = null
 var spawn: SpawnConfigData = null
 var exp_config: ExpConfig = null
-var prefabs: Dictionary = {}  # {id: MapPrefab}
 
 
 func _ready() -> void:
@@ -146,7 +145,6 @@ func _ready() -> void:
 	_load_waves_by_map("res://resources/waves/")
 	_load_resources_from_dir("res://resources/characters/", characters)
 	_load_resources_from_dir("res://resources/maps/", maps)
-	_load_resources_from_dir("res://resources/maps/prefabs/", prefabs)
 	effects = load("res://resources/effects/default_effects.tres")
 	shop = load("res://resources/shop/default_shop.tres")
 	shop_config = load("res://resources/shop/shop_config.tres")
